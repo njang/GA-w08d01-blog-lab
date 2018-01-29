90f1b1ab3a8b6a5a8448809a3dcd665274180652
njang <neo.w.jang@gmail.com>
2018-01-29 15:42:40 -0600

    Complete the blog app

--- src/Post.js
+++ src/Post.js
@@ -4,6 +4,24 @@ import Comment from './Comment.js'
 import './Post.css';
 
 class Post extends Component {
+  // when our component is initialized,
+  // our constructor function is called
+  constructor (props) {
+    // make call to parent class' (Component) constructor
+    super()
+    // define an initial state
+    this.state = {
+      // counter: 0 // initialize this.state.counter to be 0
+      body: "White whale"
+    }
+  }
+  handleClick (e) {
+    // setState is inherited from the Component class
+    let newBody = prompt("Please enter your name", "");
+    this.setState({
+      body: newBody 
+    })
+  }
   render() {
     return (
       <div className="Post">
@@ -16,7 +34,9 @@ class Post extends Component {
         </p>
         <p>{this.props.title}</p>
         <p>{this.props.author}</p>
-        <p>{this.props.body}</p>
+        <span>{this.state.body} <button onClick={(e) => this.handleClick(e)}>Edit</button></span>
+        
+        
         <h3>Comments:</h3>
         {/* Render Comment component, passing in data */}
         {this.props.comments.map((comment) => {
--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import Post from './Post';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-	<Post title={"Moby Dick"} author={"Herman Melville"} body={"White whale"} comments={["Hello world", "Good night and Good bye"]} />, 
+	<Post title={"Moby Dick"} author={"Herman Melville"} comments={["Hello world", "Good night and Good bye"]} />, 
 	document.getElementById('root')
 );
 registerServiceWorker();
